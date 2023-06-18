0bc6f6479b11b5ef8b9684f728979ba3ba65ff60
Sacramenitx <benji.u1225u@gmail.com>
2023-06-18 19:39:35 +0200

    remove unused bucket and collection

--- src/services/couchbase/mpg.ts
+++ src/services/couchbase/mpg.ts
@@ -5,7 +5,3 @@ export const couchCluster = await connect(env.COUCHBASE_URL, {
     username: env.COUCHBASE_USER,
     password: env.COUCHBASE_PASSWORD
 });
-
-export const mpgBucket = couchCluster.bucket(env.COUCHBASE_BUCKET);
-
-export const mpgCollection = mpgBucket.defaultCollection();
